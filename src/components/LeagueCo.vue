ff1befbe53231880feea74de5b0f15a63f756e2d
Gustavo Jiménez <tavo0520@hotmail.com>
2022-08-30 19:42:36 +0200

    Component game

--- src/components/LeagueCo.vue
+++ src/components/LeagueCo.vue
@@ -11,64 +11,49 @@
 </template>
 
 <script setup>
+import { defineProps } from 'vue';
 
-import { 
-	    defineProps, toRef, toRefs, watch, onMounted
-    } from 'vue';
-
-// onMounted(async () => {
-    let props = defineProps({
-        league: {
-            type: Object,
-            default: {}
-        },
-    });
-    console.log('props', props);
-
-// })
-
-
-
-    // watch(props, (newValue, oldValue) => {
-    //     console.log('newValue', newValue);
-    //     console.log('oldValue', oldValue);
-    // });
+let props = defineProps({
+    league: {
+        type: Object,
+        default: {}
+    },
+});
 
 </script>
 
-
 <style lang="scss">
-                    .league-item {
-                        display: grid;
-                        width: 100%;
-                        grid-template-columns: 70px 1fr;
-                        padding: 4px 0;
-                        border-bottom: 1px solid rgb(24 24 24 / 60%);
-                        margin: 0 0 5px 0;
+    .league-item {
+            display: grid;
+            width: 100%;
+            grid-template-columns: 70px 1fr;
+            padding: 4px 0;
+            border-bottom: 1px solid rgb(24 24 24 / 60%);
+            margin: 0 0 5px 0;
 
-                    .league-item-image {
-                            display: flex;
-                            // justify-content: center;
-                            align-items: center;
-                        img {
-                                width: 30px;
-                                height: 30px;
-                                object-fit: contain;
-                        }
-                    }
-                    .league-item-info {
-                        h3 {
-                            font-size: 13px;
-                            font-weight: bold;
-                        }
-                        p {
-                            font-size: 11px;
-                        }
-                    }
-                }
-                .league-item:hover {
-                    background: var(--colorGray);
-                    cursor: pointer;
-                    border-radius: 3px;
-                } 
+        .league-item-image {
+                display: flex;
+                // justify-content: center;
+                align-items: center;
+            img {
+                    width: 30px;
+                    height: 30px;
+                    object-fit: contain;
+            }
+        }
+        .league-item-info {
+            h3 {
+                font-size: 13px;
+                font-weight: bold;
+            }
+            p {
+                font-size: 11px;
+            }
+        }
+    }
+    .league-item:hover {
+        background: var(--colorGray);
+        cursor: pointer;
+        border-radius: 3px;
+    } 
 </style>
